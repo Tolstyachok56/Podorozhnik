4fbd9d7b161c34e5e8aa70ddc66f20bf82f42542
Viktoriia Badisova <vbadisova@gmail.com>
2019-10-16 12:48:42 +0300

    add green and back color

--- Podorozhnik/Sources/Utilities/AppColors.swift
+++ Podorozhnik/Sources/Utilities/AppColors.swift
@@ -11,9 +11,6 @@ import UIKit
 struct AppsColors {
     static let chateauGreen: UIColor = colorFromRGBA(red: 38, green: 145, blue: 91)
     
-    static let mistyRose: UIColor = colorFromRGBA(red: 255, green: 225, blue: 212)
-    static let honeydew: UIColor = colorFromRGBA(red: 234, green: 255, blue: 224)
-    
     static private func colorFromRGBA(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1.0) -> UIColor {
         return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
     }
